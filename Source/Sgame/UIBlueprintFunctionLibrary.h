5da425e2f529db22eee32fdb6f2077a86da7c855
KS Kim <65766852+simeddk@users.noreply.github.com>
2024-04-15 10:04:22 +0900

    함정들 + 와이어 액션

---
+++ Source/Sgame/UIBlueprintFunctionLibrary.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "UIBlueprintFunctionLibrary.generated.h"
+
+UCLASS()
+class SGAME_API UUIBlueprintFunctionLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintPure, meta = (WorldContext = "WorldContextObject", CallableWithoutWorldContext), Category = "HUD|Util")
+		static void FindScreenEdgeLocationForWorldLocation(UObject* WorldContextObject, const FVector& InLocation, const float EdgePercent, FVector2D& OutScreenPosition, float& OutRotationAngleDegrees, bool& bIsOnScreen);
+};
